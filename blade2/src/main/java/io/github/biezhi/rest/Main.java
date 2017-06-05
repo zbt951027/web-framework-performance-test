be2c8e47dbfb723bf3c0ef12af12bcdc944130b5
biezhi <biezhi.me@gmail.com>
2017-06-05 23:46:06 +0800

    fix blade repo url

--- blade2/src/main/java/io/github/biezhi/rest/Main.java
+++ blade2/src/main/java/io/github/biezhi/rest/Main.java
@@ -11,6 +11,6 @@ public class Main {
     public static void main(String[] args) {
         Blade.me().devMode(false)
                 .get("/hello", ((request, response) -> response.text("Hello World.")))
-                .openMonitor(false).listen(8080).start();
+                .enableMonitor(false).listen(8080).start();
     }
 }
